e18c3774886dd7d9e6e4d03a4a396479e73ed3f7
mitsuru <codeberg@yamamech.sh>
2026-01-21 00:46:37 +0000

    users: add os_detect.c

--- users/mitsuruu/rules.mk
+++ users/mitsuruu/rules.mk
@@ -13,3 +13,8 @@ endif
 ifeq ($(strip $(MITSURUU_RGBLIGHT_ENABLE)), yes)
 	SRC += lighting/lighting.c
 endif
+
+ifeq ($(strip $(OS_DETECTION_ENABLE)), yes)
+    SRC += os_detect/os_detect.c
+endif
+
