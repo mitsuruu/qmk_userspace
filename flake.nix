fd7f3bb08cdb6f3a412c888fb885f2de4c3e143b
mitsuru <codeberg@yamamech.sh>
2026-01-21 00:28:25 +0000

    chore: add devshell for macos

--- flake.nix
+++ flake.nix
@@ -3,12 +3,19 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = inputs: let
-    pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
+  outputs = {
+    nixpkgs,
+    self,
+  }: let
+    systems = ["x86_64-linux" "aarch64-darwin"];
+    forAllSystems = function:
+      nixpkgs.lib.genAttrs systems (system: function nixpkgs.legacyPackages.${system});
   in {
-    devShells.x86_64-linux.default = pkgs.mkShell {
-      name = "qmk";
-      packages = [pkgs.qmk];
-    };
+    devShells = forAllSystems (pkgs: {
+      default = pkgs.mkShell {
+        name = "qmk";
+        packages = [pkgs.qmk];
+      };
+    });
   };
 }
