91ca84e476abbdf418e93a2bc148f7f253cd53b8
mitsuru <codeberg@yamamech.sh>
2026-01-23 01:32:09 +0000

    keyboards: update binepad/bn009 keymap

--- keyboards/binepad/bn009/keymaps/mitsuruu/keymap.c
+++ keyboards/binepad/bn009/keymaps/mitsuruu/keymap.c
@@ -1,14 +1,15 @@
 #include QMK_KEYBOARD_H
+#include "mitsuruu.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT_ortho_3x3(
-        KC_7, KC_8, KC_9,
-        KC_4, KC_5, KC_6,
-        KC_1, KC_2, MO(1)
+    [_BASE] = LAYOUT_ortho_3x3(
+        KC_7,    KC_8,    KC_9,
+        KC_4,    KC_5,    KC_6,
+        KC_1,    KC_2,    FN
     ),
-    [1] = LAYOUT_ortho_3x3(
-        QK_BOOT, _______, _______,
-        _______, _______, _______,
-        _______, _______, _______
+    [_FN] = LAYOUT_ortho_3x3(
+        QK_BOOT, KC_KP_8, KC_KP_9,
+        KC_KP_4, KC_KP_5, KC_KP_6,
+        KC_KP_1, KC_KP_2, _______
     )
 };
