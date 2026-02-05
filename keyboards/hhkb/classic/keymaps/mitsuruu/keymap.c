ab9a04c894e54fddca5a5cffcc6ef8347b6deeb2
mitsuru <codeberg@yamamech.sh>
2026-02-05 21:47:24 +0000

    keyboards: replace EXT_F* with KC_F* in hhkb/classic keymap

--- keyboards/hhkb/classic/keymaps/mitsuruu/keymap.c
+++ keyboards/hhkb/classic/keymaps/mitsuruu/keymap.c
@@ -17,8 +17,8 @@
 #include QMK_KEYBOARD_H
 #include "mitsuruu.h"
 
-// clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    // clang-format off
     [_BASE] = LAYOUT_60_hhkb(
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_NUBS, KC_GRV,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, HH_BSPC,
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                  KC_LGUI, LM_LALT,                            KC_SPC,                             FN,      KC_RGUI
     ),
     [_FN] = LAYOUT_60_hhkb(
-        KC_SLEP, EXT_F1,  EXT_F2,  EXT_F3,  EXT_F4,  EXT_F5,  EXT_F6,  EXT_F7,  EXT_F8,  EXT_F9,  EXT_F10, EXT_F11, EXT_F12, KC_NUHS, KC_DEL,
+        KC_SLEP, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_DEL,
         KC_CAPS, _______, _______, _______, _______, _______, _______, _______, KC_PSCR, KC_SCRL, KC_PAUS, KC_UP,   _______, _______,
         _______, KC_VOLD, KC_VOLU, KC_MUTE, _______, _______, KC_PAST, KC_PSLS, KC_HOME, KC_PGUP, KC_LEFT, KC_RGHT,          KC_PENT,
         _______,          KC_MPRV, KC_MNXT, _______, _______, _______, KC_PPLS, KC_PMNS, KC_END,  KC_PGDN, KC_DOWN, _______, _______,
@@ -40,5 +40,5 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
                  _______, _______,                            _______,                            _______, _______
     )
+    // clang-format on
 };
-// clang-format on
