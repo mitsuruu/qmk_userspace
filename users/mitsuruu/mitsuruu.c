db00e22939c7d2fd5ff27866f2b320e63b3250dd
mitsuru <codeberg@yamamech.sh>
2026-03-20 17:55:17 +0000

    fix: fix LShift+RShift+Esc not rebooting keyboard to bootloader

--- users/mitsuruu/mitsuruu.c
+++ users/mitsuruu/mitsuruu.c
@@ -56,14 +56,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             return false;
         case KC_ESCAPE:
             if (record->event.pressed) {
-                uint8_t temp_mod = get_mods();
-                uint8_t temp_osm = get_oneshot_mods();
-                clear_mods();
-                clear_oneshot_mods();
-                if ((temp_mod | temp_osm) & (MOD_BIT(KC_LEFT_SHIFT) & MOD_BIT(KC_RIGHT_SHIFT))) {
-                    tap_code16(QK_BOOT);
+                if ((get_mods() & MOD_MASK_SHIFT) == MOD_MASK_SHIFT) {
+                    clear_mods();
+                    reset_keyboard();
+                    return false;
                 }
-                set_mods(temp_mod);
             }
             return true;
     }
